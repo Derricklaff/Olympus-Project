65761b6e2ece1dd03fbe8e09a3765702a29f858d
Gayle Hoefer <gaylehoefer@gmail.com>
2022-11-26 17:18:03 -0700

    planet model is created

--- server/models/Planet.js
+++ server/models/Planet.js
@@ -2,14 +2,33 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const categorySchema = new Schema({
+const planetSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
+  },
   name: {
     type: String,
     required: true,
-    trim: true
+  },
+  oxygenRate: {
+    type: Number,
+    required: true
+  },
+  oreRate: {
+    type: Number,
+    required: true
+  },
+  foodRate: {
+    type: Number,
+    required: true
+  },
+  waterRate: {
+    type: Number,
+    required: true
   }
 });
 
-const Category = mongoose.model('Category', categorySchema);
+const Planet = mongoose.model('Planet', planetSchema);
 
-module.exports = Category;
+module.exports = Planet;
