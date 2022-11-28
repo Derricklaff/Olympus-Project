9d1cf560e7fa56acbcbb9a3ca016f49e32fff8bc
Gayle Hoefer <gaylehoefer@gmail.com>
2022-11-28 13:38:55 -0700

    server is running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User} = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
@@ -8,9 +8,9 @@ const resolvers = {
     users: async (parent, { _id }) => {
       return await User.findById(_id).populate('room');
     },
-    
-    },
-    Mutation: {
+  },
+
+  Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
@@ -18,30 +18,31 @@ const resolvers = {
       return { token, user };
     },
 
-  },
-  updateUser: async () => {
-    if (context.user) {
-      return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+    },
+
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     }
-  },
-
-  login: async (parent, { username, password }) => {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      throw new AuthenticationError('Incorrect credentials');
-    }
-
-    const correctPw = await user.isCorrectPassword(password);
-
-    if (!correctPw) {
-      throw new AuthenticationError('Incorrect credentials');
-    }
-
-    const token = signToken(user);
-
-    return { token, user };
   }
-};
+}
 
-module.exports = resolvers;
+  module.exports = resolvers;
