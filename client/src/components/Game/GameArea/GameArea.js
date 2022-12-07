637451d42660988c49c607a3b8558095cfc52312
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-06 23:12:58 -0700

    restyled game page

--- client/src/components/Game/GameArea/GameArea.js
+++ client/src/components/Game/GameArea/GameArea.js
@@ -1,4 +1,4 @@
-import { Image, Card, CardBody, Flex, Button } from '@chakra-ui/react';
+import { Image, Card, CardBody, Flex, Button, Container } from '@chakra-ui/react';
 import background from '../../images/background.png';
 import ComputerGreen from '../../images/ComputerGreen.gif'
 import ComputerRed from '../../images/ComputerRed.gif'
@@ -6,29 +6,26 @@ import ComputerRed from '../../images/ComputerRed.gif'
 function GameArea({ onOpen, GameEnd }) {
     return (
         <>
-            <Card mt={50} bgColor='none' border='none' align='center' justify='center'>
-                <CardBody>
+            <Container mt={50} bgColor='none' border='none' align='center' justify='center'>
+                {/* <CardBody>
                     <Flex align='center' justify='center' >
                         <Image
                             rounded='3xl'
                             boxShadow='dark-lg'
                             src={background}
                             alt='Space Station'
-                            w='115em'
-                            
-                            
                         />
                     </Flex>
-                </CardBody>
+                </CardBody> */}
                 <Flex justify='center'>
-                    <Button _hover='none' bg='none' onClick={onOpen}> <Image  
-                        w='100px'
+                    <Button _hover='none' bg='none' onClick={onOpen}> <Image 
                         position= 'relative'
-                        top= '-525px'
-                        left= '280px'
+                        left= '-3px'
+                        top= '508px'
+                        w= '212px'
                     src={!GameEnd ? ComputerRed : ComputerGreen} /></Button>
                 </Flex>
-            </Card>
+            </Container>
         </>
     );
 }
